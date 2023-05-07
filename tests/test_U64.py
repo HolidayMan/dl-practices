eac941b58f0841c3c01f53bc3ead40b53f49e25b
Alex Pitenko <oleksii.p@itsyndicate.org>
2023-05-07 23:50:50 +0200

    full covered U64 with unit tests

--- tests/test_U64.py
+++ tests/test_U64.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.U64 import U64
 
 
@@ -22,3 +24,32 @@ def test_add():
     assert U64(2382398) + U64(1) == U64(2382399)
     assert U64(2**64-1) + U64(2) == U64(1)
     assert U64(2**64-1) + U64(2**64-1) == U64(2**64-2)
+
+
+def test_eq():
+    assert U64(2**64-1) == U64(2**64-1)
+    assert U64(2**64-1) == 2**64-1
+
+
+def test_floordiv():
+    assert U64(2**64-1) // U64(2) == U64(2**63-1)
+    assert U64(2**64-1) // 2 == U64(2**63-1)
+
+
+def test_repr():
+    assert repr(U64(2**64-1)) == "U64(18446744073709551615)"
+
+
+def test_hex_init_error():
+    with pytest.raises(ValueError):
+        U64.from_hex("1234567890abcdefg")
+
+
+def test_mod():
+    assert U64(2**64-1) % U64(2) == U64(1)
+    assert U64(2**64-1) % 2 == U64(1)
+
+
+def test_lower_than():
+    assert U64(0) < U64(2**64-1)
+    assert U64(0) < 2**64-1
